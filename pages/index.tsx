bf1c2a4a0b103158f73715e2a36aa6aa25867c7c
EmojiCat <emojiotaku@gmail.com>
2023-01-01 23:41:13 +0900

    시간/제목 쿼리로 받아서 처리

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,44 +1,47 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
+import { remainDatetime } from "../lib/remain-datetime";
 
 const Home: NextPage = () => {
+  const router = useRouter();
   const [partyTime, setPartyTime] = useState(false);
   const [days, setDays] = useState(0);
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
 
+  function evaluate(target) {
+    const { d, h, m, s } = remainDatetime(target);
+
+    setDays(d);
+    setHours(h);
+    setMinutes(m);
+    setSeconds(s);
+
+    if (d <= 0 && h <= 0 && m <= 0 && s <= 0) {
+      setPartyTime(true);
+    }
+  }
+
   useEffect(() => {
-    const target = new Date("12/31/2022 23:59:59");
+    if (!router.isReady) {
+      return;
+    }
 
-    const interval = setInterval(() => {
-      const now = new Date();
-      const difference = target.getTime() - now.getTime();
+    var target: Date = new Date();
+    if (router.query.date) {
+      target = new Date(router.query.date as string);
+    }
+    evaluate(target);
 
-      const d = Math.floor(difference / (1000 * 60 * 60 * 24));
-      setDays(d);
-
-      const h = Math.floor(
-        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-      );
-      setHours(h);
-
-      const m = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-      setMinutes(m);
-
-      const s = Math.floor((difference % (1000 * 60)) / 1000);
-      setSeconds(s);
-
-      if (d <= 0 && h <= 0 && m <= 0 && s <= 0) {
-        setPartyTime(true);
-      }
-    }, 1000);
+    const interval = setInterval(() => evaluate(target), 1000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [router.isReady]);
 
   return (
     <div className={styles.container}>
@@ -49,7 +52,7 @@ const Home: NextPage = () => {
 
       {partyTime ? (
         <>
-          <h1>Happy new year!</h1>
+          <h1>{router.query.title ?? "시간이 다 됐어요!"}</h1>
           {/* <video autoPlay loop muted>
             <source src="/party.mp4" />
           </video> */}
