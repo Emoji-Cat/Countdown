626d6df10d1c4d20ac29a8474e790c7517542959
EmojiCat <emojiotaku@gmail.com>
2023-01-02 13:27:38 +0900

    파라미터 관련 버그 수정 Clear 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,33 +4,35 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import { ChangeEvent, useEffect, useState } from "react";
 
-import moment, { Moment } from "moment";
+import moment from "moment";
 
 import styles from "../styles/Home.module.css";
-import { remainDatetime } from "../lib/remain-datetime";
 
 const Home: NextPage = () => {
   const router = useRouter();
   const [partyTime, setPartyTime] = useState(false);
   const [target, setTarget] = useState(moment().add(1, 'day'));
+  const [remain, setRemain] = useState(moment.duration(0));
   const [title, setTitle] = useState("");
   const [finalMessage, setFinalMessage] = useState("");
-  const [days, setDays] = useState(0);
-  const [hours, setHours] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [seconds, setSeconds] = useState(0);
 
-  function evaluate(target: Moment) {
-    const { d, h, m, s } = remainDatetime(target.toDate());
+  function clear() {
+    const aDayAfter = moment().add(1, 'day');
 
-    setDays(d);
-    setHours(h);
-    setMinutes(m);
-    setSeconds(s);
+    router.replace(
+      "/",
+      "/",
+      { shallow: true }
+    )
 
-    if (d <= 0 && h <= 0 && m <= 0 && s <= 0) {
-      setPartyTime(true);
-    }
+    setTitle("");
+    setTarget(aDayAfter);
+  }
+
+  function evaluate() {
+    const duration = target.diff(moment());
+    setRemain(moment.duration(duration));
+    setPartyTime(duration <= 0);
   }
 
   function onChangeTitle(event: ChangeEvent<HTMLInputElement>) {
@@ -43,77 +45,84 @@ const Home: NextPage = () => {
     }
 
     router.replace(
-      "/?date=" + target.format(moment.HTML5_FMT.DATETIME_LOCAL) + "&title=" + title,
-      "/?date=" + target.format(moment.HTML5_FMT.DATETIME_LOCAL) + "&title=" + title,
+      "/?date=" + target.format(moment.HTML5_FMT.DATETIME_LOCAL) + "&title=" + (title ?? ""),
+      "/?date=" + target.format(moment.HTML5_FMT.DATETIME_LOCAL) + "&title=" + (title ?? ""),
       { shallow: true }
     )
   }
 
   function onChangeDateTime(event: ChangeEvent<HTMLInputElement>) {
     const newTarget = moment(new Date(event.target.value));
+
+    setTarget(newTarget);
+    evaluate();
+
     router.replace(
       "/?date=" + newTarget.format(moment.HTML5_FMT.DATETIME_LOCAL) + "&title=" + (title ?? ""),
       "/?date=" + newTarget.format(moment.HTML5_FMT.DATETIME_LOCAL) + "&title=" + (title ?? ""),
       { shallow: true }
     )
-    setTarget(newTarget);
-    evaluate(newTarget);
   }
 
   useEffect(() => {
-    if (!router.isReady) {
+    if (router.isReady) {
+      if (router.query.title) {
+        setTitle(router.query.title as string);
+      }
+
+      if (router.query.finalMessage) {
+        setFinalMessage(router.query.finalMessage as string);
+      }
+
+      const date = moment(router.query.date as string);
+      if (router.query.date && date.isValid) {
+        setTarget(date);
+      }
+    } else {
       return;
     }
 
-    if (router.query.title) {
-      setTitle(router.query.title as string);
-    }
+    evaluate();
 
-    if (router.query.finalMessage) {
-      setFinalMessage(router.query.finalMessage as string);
-    }
-
-    if (router.query.date) {
-      const date = moment(router.query.date as string);
-      if (date.isValid) {
-        setTarget(date);
-      }
-    }
-    evaluate(target);
-
-    const interval = setInterval(() => evaluate(target), 1000);
+    const interval = setInterval(evaluate, 1000);
 
     return () => clearInterval(interval);
-  }, [router.isReady, router.query, target, title]);
+  }, [router.isReady, router.query, partyTime]);
 
   return (
     <div className={styles.container}>
       <Head>
-        <title>Countdown Timer</title>
+        <title>{Boolean(title) ? title : "Countdown Timer"}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className={styles.card}>
-        <input
-          type="text"
-          value={title}
-          placeholder="어떤 기념일인가요?"
-          onChange={(e) => onChangeTitle(e)}
-          onKeyUp={(e) => onKeyUpTitle(e)}
-        />
-        <input
-          type="datetime-local"
-          id="date"
-          name="date"
-          value={target.format(moment.HTML5_FMT.DATETIME_LOCAL)}
-          min={moment().format(moment.HTML5_FMT.DATETIME_LOCAL)}
-          onChange={(e) => onChangeDateTime(e)}
-        />
-      </div>
-
       {partyTime ? (
         <>
-          <h1>{finalMessage ?? "시간이 다 됐어요!"}</h1>
+          <h1>{Boolean(finalMessage) ? finalMessage : "시간이 다 됐어요!"}</h1>
+          <button onClick={(e) => clear()}>다시하기</button>
+        </>
+      ) : (
+        <div className={styles.card}>
+          <input
+            type="text"
+            value={title}
+            placeholder="어떤 기념일인가요?"
+            onChange={(e) => onChangeTitle(e)}
+            onKeyUp={(e) => onKeyUpTitle(e)}
+          />
+          <input
+            type="datetime-local"
+            id="date"
+            name="date"
+            value={target.format(moment.HTML5_FMT.DATETIME_LOCAL)}
+            min={moment().format(moment.HTML5_FMT.DATETIME_LOCAL)}
+            onChange={(e) => onChangeDateTime(e)}
+          />
+        </div>
+      )}
+
+      {partyTime ? (
+        <div className={styles.background}>
           {/* <video autoPlay loop muted>
             <source src="/party.mp4" />
           </video> */}
@@ -122,27 +131,27 @@ const Home: NextPage = () => {
             src="/background.png"
             fill
           />
-        </>
+        </div>
       ) : (
         <>
           <div className="timer-wrapper">
             <div className="timer-segment">
-              <span className="time">{days}</span>
+              <span className="time">{remain.days()}</span>
               <span className="label">Days</span>
             </div>
             <span className="divider">:</span>
             <div className="timer-segment">
-              <span className="time">{hours.toLocaleString('es-US', {minimumIntegerDigits: 2})}</span>
+              <span className="time">{remain.hours().toLocaleString('es-US', { minimumIntegerDigits: 2 })}</span>
               <span className="label">Hours</span>
             </div>
             <span className="divider">:</span>
             <div className="timer-segment">
-              <span className="time">{minutes.toLocaleString('es-US', {minimumIntegerDigits: 2})}</span>
+              <span className="time">{remain.minutes().toLocaleString('es-US', { minimumIntegerDigits: 2 })}</span>
               <span className="label">Minutes</span>
             </div>
             <span className="divider">:</span>
             <div className="timer-segment">
-              <span className="time">{seconds.toLocaleString('es-US', {minimumIntegerDigits: 2})}</span>
+              <span className="time">{remain.seconds().toLocaleString('es-US', { minimumIntegerDigits: 2 })}</span>
               <span className="label">Seconds</span>
             </div>
           </div>
